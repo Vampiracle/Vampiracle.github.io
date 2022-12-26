470cf30035926d39645929757ba2d6363489dcdd
Vampiracle <65716099+Vampiracle@users.noreply.github.com>
2022-12-26 14:38:40 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
 document.writeln('<nav id="my_menu">');
 document.writeln('            <ul class="menu">');
-document.writeln('                <li class="menu__link"><a href="../index.html" class="menu__a" id="menu__a__id">Главная</a></li>');
-document.writeln('                <li class="menu__link"><a href="../skills.html" class="menu__a" id="menu__a__id">Скиллы и портфолио</a></li>');
-document.writeln('                <li class="menu__link"><a href="../another.html" class="menu__a" id="menu__a__id">Контакты и др.</a></li>');
+document.writeln('                <li class="menu__link"><a href="../HTML/index.html" class="menu__a" id="menu__a__id">Главная</a></li>');
+document.writeln('                <li class="menu__link"><a href="../HTML/skills.html" class="menu__a" id="menu__a__id">Скиллы и портфолио</a></li>');
+document.writeln('                <li class="menu__link"><a href="../HTML/another.html" class="menu__a" id="menu__a__id">Контакты и др.</a></li>');
+document.writeln('                <li class="menu__link"><a href="../HTML/constructor_menu.html" class="menu__a" id="menu__a__id">5 лаба</a></li>');
+document.writeln('                <li class="menu__link"><a href="../HTML/table.html" class="menu__a" id="menu__a__id">JSON</a></li>');
 document.writeln('            </ul>');
 document.writeln('        </nav>');
 
